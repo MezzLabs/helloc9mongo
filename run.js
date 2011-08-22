627c73ce474c166eee0d3a180ea5aa6445861281
Matt <matt.pardee@gmail.com>
2011-08-22 20:45:18 +0000

    Added documentation and insert method

--- run.js
+++ run.js
@@ -2,22 +2,70 @@ var sys = require("sys");
 var mongoose = require("mongoose");
 var config = require("./config");
 
+// Assemble our connection details from config.js
 var loginCredentials = config.username + ':' + config.password;
 var dbUrl = config.databaseUrl;
 var dbName = config.databaseName;
 
+// Connect to the MongoLab database
 mongoose.connect("mongodb://" + loginCredentials + "@" + dbUrl + "/" + dbName);
 
-function find (collectionIdent, query, callback) {
+/**
+ * Queries a collection to retrieve data based on
+ * properties supplied by @json
+ * 
+ * @param {string} collectionIdent Identifier for the collection
+ * @param {JSON} json Hash of details to query against
+ * @param {function} callback Callback to return to
+ */
+function query (collectionIdent, json, callback) {
     mongoose.connection.db.collection(collectionIdent, function (err, collection) {
-        collection.find(query).toArray(callback);
+        collection.find(json).toArray(callback);
     });
 }
 
+/**
+ * Inserts data into a collection
+ * 
+ * @param {string} collectionIdent Identifier for the collection
+ * @param {JSON} data Data to insert into the collection
+ * @param {function} callback Callback to return to
+ */
+function insert (collectionIdent, data, callback) {
+    mongoose.connection.db.collection(collectionIdent, function (err, collection) {
+        collection.insert(data, callback);
+    });
+}
+
+// Detect the connection "open" event
 mongoose.connection.on("open", function() {
-    console.log("connected");
-    find('checkins', {}, function (err, docs) {
-        console.log(docs);
-        mongoose.disconnect();
+    console.log("Connected!");
+
+    // Create some fake location details to insert
+    var fakeLocation = {
+        name : "Compton",
+        location : {
+            lat : 33.899629,
+            long : -118.22053
+        }
+    };
+    
+    insert("locations", fakeLocation, function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        // Query for all locations (note empty JSON in 2nd parameter)
+        query("locations", {}, function (err, docs) {
+            if (err) {
+                console.log("Query error", err);
+                return;
+            }
+
+            // Spit out the results and disconnect
+            console.log("Query Result(s):", docs);
+            mongoose.disconnect();
+        });
     });
 });
